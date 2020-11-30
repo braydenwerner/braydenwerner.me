89b3b1d815ecbfb6b3b97d4d28783e6ab263a599
Brayden Werner <braydenwerner0@gmail.com>
2020-11-30 15:29:33 -0800

    update

--- src/components/SocialSideBar/SocialSideBar.js
+++ src/components/SocialSideBar/SocialSideBar.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { SideBar, IconImage } from '../exports'
 import { socialLinks } from '../../constants/constants'
 import './SocialSideBar.scss'
 
 export default function SocialSideBar() {
     console.log(socialLinks)
+    const [isMounted, setIsMounted] = useState(false)
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setIsMounted(true)
+        }, 100)
+
+        return () => {
+            clearTimeout(timeout)
+        }
+    }, [])
     return (
         <SideBar location="right">
             <div className="social-list">
-                {socialLinks && socialLinks.map(({ url, name }) => (
+                {isMounted && socialLinks && socialLinks.map(({ url, name }) => (
                     <li key={name}>
                         <a href={url} aria-label={name} target="_blank" rel="noreferrer">
                             <IconImage name={name} />
